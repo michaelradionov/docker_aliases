3d90a7da54782d5c58399b046ef0e4e873f4b383
radionov <boss@flagstudio.ru>
2018-09-15 15:39:56 +0500

    better dinc alias

--- docker_aliases.sh
+++ docker_aliases.sh
@@ -2,7 +2,7 @@
 alias dps='docker ps --format "table {{.ID}}\t{{.Image}}\t{{.Names}}\t{{.Size}}\t{{.RunningFor}}"'
 alias dpss='docker ps --format "table {{.ID}}\t{{.Image}}\t{{.Names}}\t{{.Mounts}}\t{{.Ports}}"'
 alias din='function _din(){ docker exec -it $1 bash; };_din'
-alias dinc='function _dinc(){ docker exec -it $1 bash -c "$2"; };_dinc'
+alias dinc='function _dinc(){ docker exec -t $1 bash -c "$2"; };_dinc'
 alias dc='docker-compose'
 
 dorm (){
