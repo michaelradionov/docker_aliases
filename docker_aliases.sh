fddd42a07dfbae3ce345b281d51fdfba30fa2418
radionov <boss@flagstudio.ru>
2018-09-15 15:55:07 +0500

    dbSizesInCont function to get DB sizes in container. What you thought it is? A cookie jar?

--- docker_aliases.sh
+++ docker_aliases.sh
@@ -5,6 +5,11 @@ alias din='function _din(){ docker exec -it $1 bash; };_din'
 alias dinc='function _dinc(){ docker exec -t $1 bash -c "$2"; };_dinc'
 alias dc='docker-compose'
 
+# Show DB sizes in container. First argument is container name, second argument is mysql password INSIDE that container
+dbSizesInCont(){
+	docker exec -t ${1} mysql -p${2} -e 'SELECT table_schema "DB Name", ROUND(SUM(data_length + index_length) / 1024 / 1024, 1) "DB Size in MB" FROM information_schema.tables GROUP BY table_schema; '
+}
+
 dorm (){
   read -p 'Stop and remove all containers? Are you sure??? [y]: ' y
 
